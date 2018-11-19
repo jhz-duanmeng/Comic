2ebf2f00aa8349e6ed6e6c8a09e9bea74b6c607f
duanmeng@ren001.com <duanmeng@ren001.com>
2018-11-19 18:09:32 +0800

    Initial commit

---
+++ app/src/main/java/com/meng/comic/ui/ZElasticRefreshScrollView.java
@@ -0,0 +1,17 @@
+package com.meng.comic.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by Dmeng on 2018/11/19.
+ */
+
+public class ZElasticRefreshScrollView extends ScrollView{
+
+
+    public ZElasticRefreshScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
